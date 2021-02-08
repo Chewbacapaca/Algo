43dd71555f7eb7e0de8ac3ffcc22d6cdadb170f6
Chewbacapaca <leatherscalpel@gmail.com>
2021-02-08 14:31:13 +0545

    updated

---
+++ a or b or c.py
@@ -0,0 +1,9 @@
+a = int(input("Enter number for a: "))
+b = int(input("Enter number for b: "))
+c = int(input("Enter number for c: "))
+if a > b and a > c:
+    print("a is largest.")
+elif b > c:
+    print("b is greatest")
+else:
+    print("c is greatest")
